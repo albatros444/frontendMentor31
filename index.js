7637e84ee44608c9f9bed65a6910009bb16709d2
alex444 <alexbatenkin@bk.ru>
2024-05-03 03:23:36 +0400

    desktop layout done (pixel perfect kinda)

---
+++ index.js
@@ -0,0 +1,23 @@
+const slider = document.querySelector("#range");
+const pageViews = document.querySelector(".pageViewsNumber");
+const price = document.querySelector(".price");
+const switchButton = document.querySelector("#switchButton");
+///////////////
+const pageViewsArr = ["10K", "50K", "100K", "500K", "1M"];
+const pricesArr = ["8.00", "12.00", "16.00", "24.00", "36.00"];
+function progressScript() {
+  const sliderValue = slider.value;
+  slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${
+    sliderValue * 25
+  }%, hsl(224, 65%, 95%) ${sliderValue}%)`;
+  pageViews.innerText = pageViewsArr[sliderValue];
+  price.innerText = switchButton.checked
+    ? Number(pricesArr[sliderValue]) * 0.75 + ".00"
+    : pricesArr[sliderValue];
+}
+progressScript();
+switchButton.addEventListener("click", () => {
+  price.innerText = switchButton.checked
+    ? Number(pricesArr[slider.value]) * 0.75 + ".00"
+    : pricesArr[slider.value];
+});
